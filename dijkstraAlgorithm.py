4b398fcfd4242810f4225f1798063567bc36da0c
Tom Prins <tomprins64@gmail.com>
2022-06-21 13:45:07 +0200

    updated so that the nodetable can derive its values from the adjecent dictionary

--- dijkstraAlgorithm.py
+++ dijkstraAlgorithm.py
@@ -8,14 +8,15 @@ adjecent = {
     "F": [("D", 2), ("E", 1), ("C", 3)]}
 
 # Define a nodeTable where the shortest routes for each nodes are stored
-nodeTable = {
-    "A": [0, None],
-    "B": [10000, None],
-    "C": [10000, None],
-    "D": [10000, None],
-    "E": [10000, None],
-    "F": [10000, None]
-}
+nodeTable = {}
+firstLetter = True
+
+for letter in adjecent:
+    if firstLetter == True:
+        nodeTable[letter] = [0, None]
+        firstLetter = False
+    else:
+        nodeTable[letter] = [10000, None]
 
 notVisited = list(nodeTable.keys())
 
@@ -27,7 +28,8 @@ for node in nodeTable:
 
     for pathes in range(len(adjecent[currentNode])):
         pathLetter = adjecent[currentNode][pathes][0]
-        totalNewPathWeight = adjecent[currentNode][pathes][1] + nodeTable[currentNode][0]
+        totalNewPathWeight = adjecent[currentNode][pathes][1] + \
+            nodeTable[currentNode][0]
         oldPathWeight = nodeTable[adjecent[currentNode][pathes][0]][0]
 
         # If an adjent node from the currentNode is not visted, check if combined wheigt is lower than before.
